4f0ea8055a062b25b9c9a2c2c1d703f4592611a3
Nttlong <zugeliang2000@gmail.com>
2025-10-09 18:20:02 +0700

    fix return error 23505 for PG

--- dialect/postgres/dialect.Postgres.ParseError.23505.go
+++ dialect/postgres/dialect.Postgres.ParseError.23505.go
@@ -6,8 +6,12 @@ import (
 
 	"github.com/lib/pq"
 )
-//23502
+
+/*
+For PostgreSQL error 23505, it is necessary to check the UniqueKeys and PrimaryKeys in the database schema for detailed inspection
+*/
 func (d *postgresDialect) ParseError23505(dbSchame *types.DbSchema, err *pq.Error) error {
+	//"23505"
 	ukContraint := err.Constraint
 	if colsInfo, ok := dbSchame.UniqueKeys[ukContraint]; ok {
 		dbCols := []string{}
@@ -24,6 +28,20 @@ func (d *postgresDialect) ParseError23505(dbSchame *types.DbSchema, err *pq.Erro
 			ConstraintName: ukContraint,
 		}
 	}
-
+	if colsInfo, ok := dbSchame.PrimaryKeys[ukContraint]; ok {
+		dbCols := []string{}
+		for _, col := range colsInfo.Columns {
+			dbCols = append(dbCols, col.Name)
+		}
+		return &errors.DbErr{
+			Err:            err,
+			ErrorType:      errors.ERR_DUPLICATE,
+			DbCols:         dbCols,
+			Table:          colsInfo.TableName,
+			Fields:         dbCols,
+			ErrorMessage:   "duplicate",
+			ConstraintName: ukContraint,
+		}
+	}
 	return err
 }
